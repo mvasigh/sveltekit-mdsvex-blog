3d6b30c0658d962983f3eda11ab9af53326ef54b
Mehdi Vasigh <mehdi.vasigh@gmail.com>
2021-05-08 23:09:14 -0400

    mdsvex initial commit

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+  const modules = import.meta.glob('./posts/*.md');
+
+  const filenameRegex = /(?<filename>.+)\.(svelte\.)?md/i;
+
+  console.log(modules);
+</script>
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,11 @@
+import { mdsvex } from "mdsvex";
+import { mdsvexConfig } from "./mdsvex.config.js";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: [".svelte", ...mdsvexConfig.extensions],
+	preprocess: [
+		mdsvex(mdsvexConfig),
+	],
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
