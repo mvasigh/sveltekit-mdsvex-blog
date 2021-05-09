4cde064ece9c742b4c30949c440f582468afe0dd
Mehdi Vasigh <mehdi.vasigh@gmail.com>
2021-05-09 10:16:40 -0400

    force prerender

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,10 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: adapter()
+		adapter: adapter(),
+		prerender: {
+			force: true
+		}
 	}
 };
 
