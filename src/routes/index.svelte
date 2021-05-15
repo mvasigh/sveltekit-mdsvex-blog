7d09f2200a12ecb4f0a30e1b44341a681e12fe96
Mehdi Vasigh <mehdi.vasigh@gmail.com>
2021-05-15 11:32:01 -0400

    add limit querystring parameter

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,6 +3,8 @@
 	 * @type {import('@sveltejs/kit').Load}
 	 */
 	export async function load({ fetch }) {
+		// Use a `limit` querystring parameter to fetch a limited number of posts
+		// e.g. fetch('posts.json?limit=5') for 5 most recent posts
 		const posts = await fetch('/posts.json').then((res) => res.json());
 
 		return {
