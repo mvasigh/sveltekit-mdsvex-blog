f3040e9d57e14eb842c2075492fbd354ffa32f0d
Mehdi Vasigh <mehdi.vasigh@gmail.com>
2021-05-22 16:54:00 -0500

    remove mdsvex.config.cjs and use ES module version

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import md from 'mdsvex';
-import { mdsvexConfig } from './mdsvex.config.js';
+import mdsvexConfig from './mdsvex.config.js';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
