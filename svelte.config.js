f03632adb780467ce83c57048e53c5650a1f3f72
Mehdi Vasigh <mehdi.vasigh@gmail.com>
2022-12-27 18:15:14 -0600

    add back adapter-static

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
