b49584f5e53e44a09cc8dbb76923f3c6fb1e4719
Mehdi Vasigh <mehdi.vasigh@gmail.com>
2022-12-27 21:41:44 -0600

    Add tests (#8)
    * add a unit test for slugFromPath
    
    * add some playwright tests
    
    * add github workflow to run tests
    
    * use pnpm in workflow file
    
    * fix some pnpm related commands:
    
    * use pnpm next-7
    
    * change setup command
    
    * try removing cache
    
    * simplify a bit
    
    * try again
    
    * try yet again
    
    * use install instead
    
    * remove node 14
    
    * add playwright installation

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,23 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
+test('index page has expected content', async ({ page }) => {
 	await page.goto('/');
-	expect(await page.textContent('h1')).toBe('Welcome to SvelteKit');
+
+	const articles = await page.$$('article');
+
+	expect(await page.textContent('h1')).toBe('SvelteKit + MDsveX Blog');
+	expect(articles.length).toBeGreaterThan(0);
+	expect(articles.length).not.toBeGreaterThan(10);
+	for (const article of articles) {
+		expect(await article.$('a')).not.toBeFalsy();
+	}
+});
+
+test('clicking on article title in home page navigates to the article', async ({ page }) => {
+	await page.goto('/');
+	const title = await page.textContent('article h3');
+
+	await page.getByText(title || '').click();
+
+	expect(await page.textContent('h2')).toBe(title);
 });
